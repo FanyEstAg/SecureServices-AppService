bf3ee0852dbe1d9497024850557ebfb5109c8043
Jose Estefania Estrada Aguilar <72044829+FanyEstAg@users.noreply.github.com>
2021-07-04 15:48:24 -0500

    Archivos de la página web

---
+++ enviar1.php
@@ -0,0 +1,24 @@
+<?php
+$Nombre = $_POST['Nombre'];
+$mail = $_POST['Email'];
+$Telefono = $_POST['Telefono'];
+$message = $_POST['Incidente']
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $Nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Telefono: " . $_POST['Telefono'] . " \r\n";
+$mensaje .= "Incidente y detalles" . $_POST['Incidente']
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'lupiss_baezz@outlook.com';
+$asunto = 'Mensaje de mi sitio web';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:index.html");
+?>
